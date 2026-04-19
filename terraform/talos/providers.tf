f938873e3aec74ea04d7cc2762297ef5970ac770
Ryan Mills <mr.rcmills@gmail.com>
2026-04-19 12:30:00 +1000

    Update providers.tf
    updated the endpoint for proxmox to https://10.1.10.12:8006/

--- terraform/talos/providers.tf
+++ terraform/talos/providers.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 provider "proxmox" {
-  endpoint  = "https://10.1.1.42:8006/"
+  endpoint  = "https://10.1.10.12:8006/"
   api_token = "YOUR_TOKEN_ID=YOUR_SECRET"
   insecure  = true 
 }
